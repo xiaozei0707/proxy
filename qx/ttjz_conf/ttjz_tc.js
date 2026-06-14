69c5a513ba6f8d23978dcb3a31933edf982121cc
jules. <144853724+xiaozei0707@users.noreply.github.com>
2026-06-14 22:58:54 +0800

    Update ttjz_tc.js

--- qx/ttjz_conf/ttjz_tc.js
+++ qx/ttjz_conf/ttjz_tc.js
@@ -1,12 +1,7 @@
 let obj = JSON.parse($response.body);
 
 if (obj.info && Array.isArray(obj.info.openVos)) {
-  obj.info.openVos = obj.info.openVos.filter(item =>
-    item.id !== 27 &&
-    item.openId !== 3 &&
-    item.id !== 31 &&
-    item.openId !== 6
-  );
+  obj.info.openVos = [];
 }
 
 $done({ body: JSON.stringify(obj) });
